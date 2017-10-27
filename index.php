9122d6582d70a57b772941d04bee52fc034a04ad
dirtypig8 <30851750+dirtypig8@users.noreply.github.com>
2017-10-27 16:47:49 +0800

    test two

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <title>Bootswatch: Paper</title>
 	
     <?php include( "css.php");?>
-	<!--<script
+	<script
 	  src="https://code.jquery.com/jquery-3.2.1.min.js"
 	  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 	  crossorigin="anonymous">
-	</script>-->
+	</script>
   
 	
     <style>/*
